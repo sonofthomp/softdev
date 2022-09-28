d7373fd162116da7f76e5a930cc6b2b47975e5fc
Gabriel Thompson <gthompson30@stuy.edu>
2022-09-27 22:37:42 -0400

    Update krewes.py

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,14 +1,24 @@
 """
 Chicken Fries (Mahir Riki, Vivian Teo, Gabriel Thompson)
 SoftDev
-K04 -- python file that randomly generates a devopler
+K04 -- python file that randomly generates a developer and their period from a
+       dictionary. The keys in the dictionary are periods, and each key has a value
+       of lists with developer names. The program first randomly selects a key from
+       the dictionary using random.choice, then randomly selects a name from the
+       list corresponding with that period using random.choice. Finally, the program
+       prints out both.
 2022-09-22
-time spent: 0.3 hours
+time spent: 0.5 hours
 """
-krewes = {2:["a2","b2","c2"], 7:["a7","b7","c7"], 8:["a8","b8","c8"]}
-
+import dictionary
 import random
-def randomDevo(krewes):
-    return random.choice(random.choice(list(krewes.values())))
 
-print(randomDevo(krewes))
+def randomDevo(krewes):
+# printing only the devo's name
+    # print(random.choice(random.choice(list(krewes.values()))))
+# printing both the devo's name and period
+    key = random.choice(list(dict.keys(krewes)))
+    name = random.choice(list(krewes[key]))
+    print(str(key)+", "+name)
+    
+(randomDevo(dictionary.krewes))
