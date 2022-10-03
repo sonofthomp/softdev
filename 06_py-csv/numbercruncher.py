06cba6f70a48e3d655dc87f6d240091124491c99
Gabriel Thompson <gthompson30@stuy.edu>
2022-10-03 08:07:19 -0400

    Update numbercruncher.py

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,3 +1,28 @@
+'''
+GAGI - Gitae Park & Gabriel Thompson
+SoftDev
+K06 -- StI/O: Divine your Destiny!
+2022-09-30
+.3 hrs
+OP Summary for randomdevo()
+1) Read the csv file
+2) Split the txt at all "\n" into a list of occupation and percentage
+3) Create a dictionary
+4) Iterate through the list and split at "," for occupation names without commas and "\"," with occupation names with commas into a list occupation and percentage
+5) assign occupation as key and percentage as value in dictionary
+6) save total percentage, pop the first key and last key
+7) produce a random float between 0 and total
+8) start a running sum
+9) go through each key and value, add the value to the running sum, and check if the running sum is larger than the random float
+10) if larger, than print occupation
+Q/C/C
+Is there a better way to manage the commas insied the quotation marks?
+DISCO
+.pop()
+can interate through keys ofd dictionary just by for x in dict
+'''
+
+
 import random
 
 opened_file = open('occupations.csv', 'r')
